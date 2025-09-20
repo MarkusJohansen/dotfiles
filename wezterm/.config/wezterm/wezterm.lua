d3db8de7332e08fc18f929c4231df6f5af85973b
Markus A. R. Johansen <markusarj@gmail.com>
2025-09-20 10:37:36 +0200

    wezterm keybind module

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,68 +7,17 @@ for k, v in pairs(require("appearance")) do
   config[k] = v
 end
 
+-- load keybinds module
+for k, v in pairs(require("keybinds")) do
+  config[k] = v
+end
+
 -- Window behavior
 config.initial_cols = 120
 config.initial_rows = 30
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_wayland = true
-
--- Key bindings: override default copy/paste
-config.keys = {
-  -- CTRL+c → Copy
-  {
-    key = "c",
-    mods = "CTRL",
-    action = act.CopyTo "Clipboard",
-  },
-  -- CTRL+v → Paste
-  {
-    key = "v",
-    mods = "CTRL",
-    action = act.PasteFrom "Clipboard",
-  },
-  -- ALT+A select all
-  {
-    key = "a",
-    mods = "CTRL",
-    action = act.Multiple{
-      act.ActivateCopyMode,
-      act.CopyMode("MoveToScrollbackTop"),
-      act.CopyMode({ SetSelectionMode = "Cell" }),
-      act.CopyMode("MoveToScrollbackBottom"),
-      act.CopyTo("Clipboard"),
-      act.ClearSelection,
-      act.CopyMode("Close"),
-    },
-  },
-  -- CTRL++ horizontal split
-  {
-     key="+",
-     mods="CTRL",
-     action=act{
-	     SplitHorizontal={
-		     domain="CurrentPaneDomain"
-	     }
-     }
-  },
-  -- ALT+\ Vertical split
-  {
-     key="\\",
-     mods="CTRL",
-     action=act{
-	     SplitVertical={
-		     domain="CurrentPaneDomain"
-	     }
-     }
-  },
-  -- ALT+W close current tab
-  {
-    key = "w",
-    mods = "CTRL",
-    action = act.CloseCurrentPane { confirm = true },
-  },
-}
 
 return config
 
